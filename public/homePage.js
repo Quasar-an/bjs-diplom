f34e431d596ef946b9b22e2197dc0f172b3dbc73
Angelina <pankova1797@gmail.com>
2024-11-25 17:29:01 +0300

    исправление ошибок

--- public/homePage.js
+++ public/homePage.js
@@ -1,114 +1,101 @@
+'use strict';
+
 const logoutButton = new LogoutButton();
 logoutButton.action = () => {
     ApiConnector.logout(response => {
         if (response.success) {
             location.reload();
-        } else {
-            console.error(response.error);
         }
     });
 };
 
+ApiConnector.current(response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+    }
+});
+
 const ratesBoard = new RatesBoard();
 
-function updateRates() {
+const updateRates = () => {
     ApiConnector.getStocks(response => {
         if (response.success) {
             ratesBoard.clearTable();
             ratesBoard.fillTable(response.data);
-        } else {
-            console.error(response.error);
         }
     });
-}
+};
 
 updateRates();
 setInterval(updateRates, 60000);
 
-
-const profileWidget = new ProfileWidget();
-
-ApiConnector.current(response => {
-    if (response.success) {
-        profileWidget.showProfile(response.data);
-    } else {
-        console.error('Ошибка получения данных пользователя:', response.error);
-    }
-});
-
-
 const moneyManager = new MoneyManager();
 
-moneyManager.addMoneyCallback = data => {
+moneyManager.addMoneyCallback = (data) => {
     ApiConnector.addMoney(data, response => {
         if (response.success) {
             ProfileWidget.showProfile(response.data);
-            moneyManager.setMessage(true, 'Пополнение прошло успешно!');
+            moneyManager.setMessage(true, 'Баланс успешно пополнен');
         } else {
             moneyManager.setMessage(false, response.error);
         }
     });
 };
 
-
-moneyManager.conversionMoneyCallback = data => {
-    ApiConnector.convertMoney(data, response => {
+moneyManager.conversionMoneyCallback = (data) => {
+        ApiConnector.convertMoney(data, response => {
         if (response.success) {
             ProfileWidget.showProfile(response.data);
-            moneyManager.setMessage(true, 'Конвертация выполнена успешно!');
+            moneyManager.setMessage(true, 'Конвертация выполнена успешно');
         } else {
             moneyManager.setMessage(false, response.error);
         }
     });
 };
 
-
-moneyManager.sendMoneyCallback = data => {
+moneyManager.sendMoneyCallback = (data) => {
     ApiConnector.transferMoney(data, response => {
         if (response.success) {
             ProfileWidget.showProfile(response.data);
-            moneyManager.setMessage(true, 'Перевод выполнен успешно!');
+            moneyManager.setMessage(true, 'Перевод выполнен успешно');
         } else {
             moneyManager.setMessage(false, response.error);
         }
     });
 };
 
+const favorites = new FavoritesWidget();
 
-const favoritesWidget = new FavoritesWidget();
-
-ApiConnector.getFavorites(response => {
-    if (response.success) {
-        favoritesWidget.clearTable();
-        favoritesWidget.fillTable(response.data);
-        moneyManager.updateUsersList(response.data);
-    } else {
-        console.error(response.error);
-    }
-});
-
-favoritesWidget.addUserCallback = data => {
-    ApiConnector.addUserToFavorites(data, response => {
+const updateFavorites = () => {
+    ApiConnector.getFavorites(response => {
         if (response.success) {
-            favoritesWidget.clearTable();
-            favoritesWidget.fillTable(response.data);
+            favorites.clearTable();
+            favorites.fillTable(response.data);
             moneyManager.updateUsersList(response.data);
-            favoritesWidget.setMessage(true, 'Пользователь добавлен!');
-        } else {
-            favoritesWidget.setMessage(false, response.error);
         }
     });
 };
 
-favoritesWidget.removeUserCallback = data => {
+updateFavorites();
+
+favorites.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites(data, response => {
+        if (response.success) {
+            updateFavorites();
+            favorites.setMessage(true, 'Пользователь успешно добавлен в избранное');
+        } else {
+            favorites.setMessage(false, response.error);
+        }
+    });
+};
+
+favorites.removeUserCallback = (data) => {
     ApiConnector.removeUserFromFavorites(data, response => {
         if (response.success) {
-            favoritesWidget.clearTable();
-            favoritesWidget.fillTable(response.data);
-            moneyManager.updateUsersList(response.data);
-            favoritesWidget.setMessage(true, 'Пользователь удалён!');
+            updateFavorites();
+            favorites.setMessage(true, 'Пользователь успешно удален из избранного');
         } else {
-            favoritesWidget.setMessage(false, response.error);
+            favorites.setMessage(false, response.error);
         }
     });
 };
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,73 +1,23 @@
-"use strict";
-
-class UserForm {
-    constructor() {
-        this.loginForm = document.getElementById("login");
-        this.registerForm = document.getElementById("register");
-
-        this.init();
-    }
-
-    init() {
-        if (this.loginForm) {
-            this.loginForm.addEventListener("submit", (event) => {
-                event.preventDefault();
-                const formData = new FormData(this.loginForm);
-                const login = formData.get("email");
-                const password = formData.get("password");
-
-                if (this.loginFormCallback) {
-                    this.loginFormCallback({ login, password });
-                }
-            });
-        }
-
-        if (this.registerForm) {
-            this.registerForm.addEventListener("submit", (event) => {
-                event.preventDefault();
-                const formData = new FormData(this.registerForm);
-                const login = formData.get("email");
-                const password = formData.get("password");
-
-                if (this.registerFormCallback) {
-                    this.registerFormCallback({ login, password });
-                }
-            });
-        }
-    }
-
-
-    setLoginFormCallback(callback) {
-        this.loginFormCallback = callback;
-    }
-
-    setRegisterFormCallback(callback) {
-        this.registerFormCallback = callback;
-    }
-
-    displayError(message) {
-        alert(`Ошибка: ${message}`);
-    }
-}
+'use strict';
 
 const userForm = new UserForm();
 
-userForm.setLoginFormCallback((data) => {
-    ApiConnector.login(data, (response) => {
+userForm.loginFormCallback = function(data) {
+    ApiConnector.login(data, response => {
         if (response.success) {
-            console.log("Успешная авторизация", response);
+            location.reload();
         } else {
-            userForm.displayError(response.error || "Неверный логин или пароль.");
+            userForm.setLoginErrorMessage(response.error);
         }
     });
-});
+};
 
-userForm.setRegisterFormCallback((data) => {
-    ApiConnector.register(data, (response) => {
+userForm.registerFormCallback = function(data) {
+    ApiConnector.register(data, response => {
         if (response.success) {
-            console.log("Успешная регистрация", response);
-            } else {
-            userForm.displayError(response.error || "Ошибка регистрации.");
+            location.reload();
+        } else {
+            userForm.setRegisterErrorMessage(response.error);
         }
     });
-});
+};
